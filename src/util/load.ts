457b4f4e6e577b1c75e12e14bd5366a90cc578f9
oriel-beck <orielbeck@gmail.com>
2023-10-12 13:24:38 +0300

    chore: use the register function to get the command type

--- src/util/load.ts
+++ src/util/load.ts
@@ -30,7 +30,9 @@ export async function loadCommands(
     } else {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       const controllerInstance = await getController<ApplicationCommandController>(`file://${join(path, dirent.name)}`);
-      const route = genRoute("commands", controllerInstance.type.toString(), dirent.name === 'index.js' ? '' : dirent.name.replace('.js', ''));
+      const commandData = controllerInstance.register();
+      if (!commandData) continue;
+      const route = genRoute("commands", commandData.toJSON().type?.toString() || "1", dirent.name === 'index.js' ? '' : dirent.name.replace('.js', ''));
       store.set(route, controllerInstance);
       loadRoute(publicKey, router, route, controllerInstance);
     }
